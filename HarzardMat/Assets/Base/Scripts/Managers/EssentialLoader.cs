3bc080b6a852fe0359eec099cdbdc44f7d4b9f3a
TaaviM88Laptop <juomakuski@hotmail.com>
2020-02-03 12:59:01 +0200

    Alustus. Kansiot lisätty. Manager  scriptejä tehty. Temp viholliset lisätty

---
+++ HarzardMat/Assets/Base/Scripts/Managers/EssentialLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssentialLoader : MonoBehaviour
+{
+    public GameObject UIScreen;
+    public GameObject player;
+    public GameObject gameManager;
+    public GameObject audioManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       // InstantiateUICanvas();
+       // InstantiatePlayer();
+        InstantiateGameManager();
+        InstantiateAudioManager();
+    }
+
+    private void InstantiateAudioManager()
+    {
+        if (AudioManager.Instance == null)
+        {
+            Instantiate(audioManager);
+        }
+    }
+
+    private void InstantiateGameManager()
+    {
+        if (GameManager.Instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+
+    private void InstantiateUICanvas()
+    {
+       /* if (UIManager.Instance == null)
+        {
+            //UIFade.Instance = Instantiate(UIScreen).GetComponent<UIFade>();
+            Instantiate(UIScreen);
+        }*/
+    }
+
+    private void InstantiatePlayer()
+    {
+        if (PlayerManager.Instance == null)
+        {
+            GameObject start = GameObject.Find("LevelStartPoint");
+
+            GameObject clone = Instantiate(player, start.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("player found");
+        }
+    }
+}
---
+++ HarzardMat/Assets/Base/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
