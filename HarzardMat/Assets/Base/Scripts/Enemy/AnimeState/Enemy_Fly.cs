fe2de87a0fc3d2e33ab9da86ff1572d8a809d198
Mutti88 <juomakuski@hotmail.com>
2020-03-25 19:58:57 +0200

    lepakan sini aalto paske taitaa toimia nyt. Saatana

--- HarzardMat/Assets/Base/Scripts/Enemy/AnimeState/Enemy_Fly.cs
+++ HarzardMat/Assets/Base/Scripts/Enemy/AnimeState/Enemy_Fly.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy_Fly : StateMachineBehaviour
 {
     public float speed = 3f;
+    [Header("Speed  of sine movement")]
+    public float frequency = 20.0f;
+    [Header("Size of sine movement")]
+    public float magnitude = 0.5f;
+    private Vector3 axis;
+    private Vector3 pos;
+    float sinIndex = 1;
     Rigidbody2D rb2d;
     EnemyPathfinding pathfind;
     EnemyAI AI;
@@ -12,11 +19,15 @@ public class Enemy_Fly : StateMachineBehaviour
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+       
         rb2d = animator.GetComponent<Rigidbody2D>();
         pathfind = animator.GetComponent<EnemyPathfinding>();
         AI = animator.GetComponent<EnemyAI>();
         AI.SetState(EnemyAIState.flyforward);
         emanager = animator.GetComponent<EnemyManager>();
+        //pos = rb2d.position;
+        pos = rb2d.transform.right;
+        axis = rb2d.transform.up;
         //player = pathfind.GetTargetTransform();
     }
 
@@ -24,9 +35,9 @@ public class Enemy_Fly : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
          if(AI.GetState() ==EnemyAIState.flyforward)
-         {
-            rb2d.velocity = (-1*emanager.side * Vector2.right * speed);
-         }
+         {       
+            rb2d.velocity = new Vector2( - 1 * emanager.side * speed * pos.x  ,axis.y * Mathf.Sin(Time.time * frequency) * Time.deltaTime * magnitude);
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -37,7 +48,7 @@ public class Enemy_Fly : StateMachineBehaviour
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
+    //{ 
     //    // Implement code that processes and affects root motion
     //}
 
